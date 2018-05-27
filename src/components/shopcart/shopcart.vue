fd35beb8a37fdb45ff69c4e510cd052332503fc6
Jasonzzc <489281085@qq.com>
2018-05-27 11:27:12 +0800

    优化总价格精度丢失问题

--- src/components/shopcart/shopcart.vue
+++ src/components/shopcart/shopcart.vue
@@ -17,6 +17,15 @@
         </div>
       </div>
     </div>
+    <div class="ball-container" name="drop">
+      <div v-for="(ball, index) in balls" :key="index">
+      <transition name="drop" @before-enter="beforeDrop" @enter="dropping" @after-enter="afterDrop">
+        <div class="ball" v-show="ball.show">
+          <div class="inner inner-hook"></div>
+        </div>
+      </transition>
+      </div>
+    </div>
   </div>
 </template>
 <script type="text/ecmascript-6">
@@ -25,12 +34,7 @@ export default {
     selectFoods: {
       type: Array,
       default () {
-        return [
-          {
-            price: 30,
-            count: 1
-          }
-        ]
+        return []
       }
     },
     deliveryPrice: {
@@ -42,11 +46,44 @@ export default {
       default: 0
     }
   },
+  data () {
+    return {
+      balls: [
+        {
+          show: false
+        },
+        {
+          show: false
+        },
+        {
+          show: false
+        },
+        {
+          show: false
+        },
+        {
+          show: false
+        }
+      ],
+      dropBalls: []
+    }
+  },
   computed: {
     totalPrice () {
       let total = 0
       this.selectFoods.forEach((food) => {
-        total += food.price * food.count
+        // 优化精度丢失解决方案
+        let foodPrice = food.price
+        let foodCount = food.count
+        if (foodPrice >= 0 && foodPrice < 100) {
+          total += ((foodPrice * 10) * foodCount) / 10
+        } else if (foodPrice >= 100 && foodPrice < 1000) {
+          total += ((foodPrice * 100) * foodCount) / 100
+        } else {
+          console.log('精度丢失')
+        }
+        // 解决精度丢失问题(原始方案)
+        // total += ((food.price * 10) * food.count) / 10
       })
       return total
     },
@@ -72,6 +109,54 @@ export default {
         return 'not-enough'
       } else {
         return 'enough'
+      }
+    }
+  },
+  methods: {
+    drop (el) {
+      for (let i = 0; i < this.balls.length; i++) {
+        let ball = this.balls[i]
+        if (!ball.show) {
+          ball.show = true
+          ball.el = el
+          this.dropBalls.push(ball)
+          return
+        }
+      }
+    },
+    beforeDrop (el) {
+      let count = this.balls.length // 把所有true的小球取出来
+      while (count--) {
+        let ball = this.balls[count]
+        if (ball.show) {
+          let rect = ball.el.getBoundingClientRect()
+          let x = rect.left - 32
+          let y = -(window.innerHeight - rect.top - 22)
+          el.style.display = ''
+          el.style.webkitTransform = `translate3d(0, ${y}px, 0)`
+          el.style.transform = `translate3d(0, ${y}px, 0)`
+          let inner = el.getElementsByClassName('inner-hook')[0]
+          inner.style.webkitTransform = `translate3d(${x}px, 0, 0)`
+          inner.style.Transform = `translate3d(${x}px, 0, 0)`
+        }
+      }
+    },
+    dropping (el) {
+      /* eslint-disable no-unused-vars */
+      let rf = el.offsetHeight //  HTMLElement.offsetHeight 是一个只读属性，它返回该元素的像素高度，高度包含该元素的垂直内边距和边框，且是一个整数。
+      this.$nextTick(() => {
+        el.style.webkitTransform = 'translate3d(0, 0, 0)'
+        el.style.transform = 'translate3d(0, 0, 0)'
+        let inner = el.getElementsByClassName('inner-hook')[0]
+        inner.style.webkitTransform = 'translate3d(0, 0, 0)'
+        inner.style.Transform = 'translate3d(0, 0, 0)'
+      })
+    },
+    afterDrop (el) {
+      let ball = this.dropBalls.shift()
+      if (ball) {
+        ball.show = false
+        el.style.display = 'none'
       }
     }
   }
@@ -165,4 +250,17 @@ export default {
         &.enough
           background: #00b43c
           color: #ffffff
+  .ball-container
+    .ball
+      position: fixed
+      left: 32px
+      bottom: 22px
+      z-index: 200
+      transition: all 0.4s cubic-bezier(0.49, -0.29, 0.75, 0.41)
+      .inner
+        width: 16px
+        height: 16px
+        border-radius: 50%
+        background: rgb(0, 160, 220)
+        transition: all 0.4s linear
 </style>
